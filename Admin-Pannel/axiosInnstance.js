83e324d37eaff23d56befd3c5c3c8f5c04c29449
AryaVeer0505 <aryaveerkanwar11458@gmail.com>
2025-06-03 10:04:06 +0530

    timeout

--- Admin-Pannel/axiosInnstance.js
+++ Admin-Pannel/axiosInnstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const axiosInstance=axios.create({
     baseURL:"https://real-estate-platform-backend.onrender.com",
-    timeout:10000,
+    timeout:1000,
 })
 
 // Request interceptors 
